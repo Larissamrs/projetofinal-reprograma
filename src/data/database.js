f4c270bb805b9e8465b8c4ef633614358f071a04
Larissa <larissamrs99@gmail.com>
2021-09-11 10:58:35 -0300

    ta rodando!

--- src/data/database.js
+++ src/data/database.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const mongoose = require('mongoose')
 
-const MONGODB_URL = process.env.MONGODB_URI || 'mongodb://localhost:80000/mao-dadas';
+const MONGODB_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/maosdadas';
 
-const connect = () => {mongoose.connect(MONGODB_URL, {
+const connect = () => {mongoose.connect("mongodb+srv://admin:admin@maosdadas.zn4w4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
 useNewUrlParser: true,
 useUnifiedTopology: true
 })
